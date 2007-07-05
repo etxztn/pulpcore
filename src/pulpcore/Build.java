f8b3c547e71756d4bb4917e8ea221c10eda0ed4c
brackeen <brackeen@localhost>
2007-07-05 02:18:53 +0000

    Build 215

--- src/pulpcore/Build.java
+++ src/pulpcore/Build.java
@@ -13,7 +13,7 @@
             public static final String VERSION = "0.9";
             
             /** The build number of this PulpCore build. */
-            public static final String BUILD_NUMBER = "210";
+            public static final String BUILD_NUMBER = "215";
             
             /** The platform name of this PulpCore build. */
             public static final String PLATFORM = "applet";
