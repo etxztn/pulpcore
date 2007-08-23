e25de1bf9ecb87889c4b694327e7eb96e12270ef
brackeen <brackeen@localhost>
2007-08-23 22:10:17 +0000

    Build 216

--- src/pulpcore/Build.java
+++ src/pulpcore/Build.java
@@ -13,7 +13,7 @@
             public static final String VERSION = "0.9";
             
             /** The build number of this PulpCore build. */
-            public static final String BUILD_NUMBER = "215";
+            public static final String BUILD_NUMBER = "216";
             
             /** The platform name of this PulpCore build. */
             public static final String PLATFORM = "applet";
