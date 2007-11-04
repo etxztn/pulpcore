6d09362ac91a4adef237899fea8dc12814049cd9
brackeen <brackeen@localhost>
2007-11-04 22:57:47 +0000

    Fixed Build 0.10.5

--- src/pulpcore/Build.java
+++ src/pulpcore/Build.java
@@ -13,7 +13,7 @@
             public static final String VERSION = "0.10.5";
             
             /** The build number of this PulpCore build. */
-            public static final String BUILD_NUMBER = "125";
+            public static final String BUILD_NUMBER = "148";
             
             /** The platform name of this PulpCore build. */
             public static final String PLATFORM = "applet";
