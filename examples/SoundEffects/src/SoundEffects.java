deb280de13530a340a03d8b287ae52ced2a8bb04
brackeen <brackeen@localhost>
2007-11-06 05:21:13 +0000

    Added mute button to sound effects example

--- examples/SoundEffects/src/SoundEffects.java
+++ examples/SoundEffects/src/SoundEffects.java
@@ -1,8 +1,11 @@
 // Plays sounds with level and pan animation.
 import pulpcore.animation.Fixed;
+import pulpcore.CoreSystem;
+import pulpcore.image.CoreImage;
 import pulpcore.Input;
 import pulpcore.scene.Scene2D;
 import pulpcore.sound.SoundClip;
+import pulpcore.sprite.Button;
 import pulpcore.sprite.FilledSprite;
 import pulpcore.sprite.ImageSprite;
 import pulpcore.Stage;
@@ -11,11 +14,15 @@ public class SoundEffects extends Scene2D {
     
     SoundClip boopSound, wooshSound;
     ImageSprite ear;
+    Button muteButton;
     
     public void load() {
         ear = new ImageSprite("ear.png", 300, 180);
+        muteButton = new Button(CoreImage.load("mute.png").split(3,2), 600, 440, true);
+        muteButton.setSelected(!CoreSystem.isMute());
         add(new FilledSprite(0xffffff));
         add(ear);
+        add(muteButton);
         Input.setCursor(Input.CURSOR_CROSSHAIR);
         
         boopSound = SoundClip.load("boop.wav");
@@ -24,6 +31,9 @@ public class SoundEffects extends Scene2D {
     }
     
     public void update(int elapsedTime) {
+        if (muteButton.isClicked()) {
+            CoreSystem.setMute(!muteButton.isSelected());
+        }
         if (ear.isMousePressed()) {
             // Animate from left to right speaker
             Fixed level = new Fixed(1);
@@ -32,7 +42,7 @@ public class SoundEffects extends Scene2D {
             
             wooshSound.play(level, pan);
         }
-        else if (Input.isMousePressed()) {
+        if (Input.isMousePressed() && !ear.isMouseOver() && !muteButton.isMouseOver()) {
             // Set the pan of the sound based on the mouse position
             int x = Input.getMousePressX();
             double w = Stage.getWidth() / 2;
