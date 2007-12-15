b2ed75cdbb3c6cd391e62bf87837dce4934fbcc5
brackeen <brackeen@localhost>
2007-12-15 06:38:38 +0000

    Using new Java 5 language features

--- templates/quick/src/HelloWorld.java
+++ templates/quick/src/HelloWorld.java
@@ -1,15 +1,16 @@
 import pulpcore.image.CoreFont;
-import pulpcore.Input;
 import pulpcore.scene.Scene2D;
 import pulpcore.sound.SoundClip;
 import pulpcore.sprite.ImageSprite;
 import pulpcore.sprite.Label;
 import pulpcore.sprite.Sprite;
+import static pulpcore.Input.*;
 
 public class HelloWorld extends Scene2D {
     
     Label label;
     
+    @Override
     public void load() {
         add(new ImageSprite("background.png", 0, 0));
         
@@ -22,8 +23,9 @@ public class HelloWorld extends Scene2D {
         sound.play();
     }
     
+    @Override
     public void update(int elapsedTime) {
-        double angle = 0.006 * (Input.getMouseX() - 320);
+        double angle = 0.006 * (getMouseX() - 320);
         int duration = 100;
         label.angle.animateTo(angle, duration);
     }
