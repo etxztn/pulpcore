41f5f1d9e58f35bebd711831c4caac9675f7f9bc
brackeen <brackeen@localhost>
2007-12-31 04:23:23 +0000

    Updated Widgets example to include a slider

--- examples/Widgets/src/Widgets.java
+++ examples/Widgets/src/Widgets.java
@@ -10,6 +10,7 @@ import pulpcore.sprite.Button;
 import pulpcore.sprite.FilledSprite;
 import pulpcore.sprite.Group;
 import pulpcore.sprite.Label;
+import pulpcore.sprite.Slider;
 import pulpcore.sprite.Sprite;
 import pulpcore.sprite.TextField;
 import pulpcore.Stage;
@@ -21,6 +22,7 @@ public class Widgets extends Scene2D {
     TextField passwordField;
     Button okButton;
     Button checkbox;
+    Slider slider;
     Group form;
     
     @Override
@@ -42,26 +44,35 @@ public class Widgets extends Scene2D {
         passwordField.setPasswordMode(true);
         passwordField.setAnchor(Sprite.WEST);
         
-        CoreImage buttonImage = CoreImage.load("button.png");
-        okButton = new Button(buttonImage.split(3), 0, 120);
-        okButton.setAnchor(Sprite.NORTH);
-        okButton.setKeyBinding(Input.KEY_ENTER);
+        Slider slider = new Slider("slider.png", "slider-thumb.png", 0, 80);
+        slider.setInsets(0, 1, 0, 1);
+        slider.setAnchor(Sprite.WEST);
+        Label label3 = new Label("Value: %d ", 0, 80);
+        label3.setFormatArg(slider.value);
+        label3.setAnchor(Sprite.EAST);
         
         CoreImage checkboxImage = CoreImage.load("checkbox.png");
         checkbox = Button.createLabeledToggleButton(checkboxImage.split(3,2), font,
-            "I'm feeling slanted", 0, 80, 30, 12, Sprite.WEST, false);
+            "I'm feeling slanted", 0, 120, 30, 12, Sprite.WEST, false);
         checkbox.setCursor(Input.CURSOR_DEFAULT);
         checkbox.setAnchor(Sprite.WEST);
+        
+        CoreImage buttonImage = CoreImage.load("button.png");
+        okButton = new Button(buttonImage.split(3), 0, 160);
+        okButton.setAnchor(Sprite.NORTH);
+        okButton.setKeyBinding(Input.KEY_ENTER);
         
         // Add the form fields to a group
         form = new Group(Stage.getWidth() / 2, Stage.getHeight() / 2);
         form.setAnchor(Sprite.CENTER);
         form.add(label);
-        form.add(label2);
         form.add(createTextFieldBackground(textField));
         form.add(textField);
+        form.add(label2);
         form.add(createTextFieldBackground(passwordField));
         form.add(passwordField);
+        form.add(label3);
+        form.add(slider);
         form.add(okButton);
         form.add(checkbox);
         form.pack();
