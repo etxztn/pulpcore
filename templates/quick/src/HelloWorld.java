9a18da1535044c22c06579de66a99ace0cd33995
brackeen <brackeen@localhost>
2008-12-06 21:26:07 +0000

    Removed static imports

--- templates/quick/src/HelloWorld.java
+++ templates/quick/src/HelloWorld.java
@@ -2,13 +2,13 @@ import pulpcore.scene.Scene2D;
 import pulpcore.sprite.FilledSprite;
 import pulpcore.sprite.ImageSprite;
 import pulpcore.sprite.Label;
-import static pulpcore.image.Colors.*;
+import pulpcore.image.Colors;
 
 public class HelloWorld extends Scene2D {
 
     @Override
     public void load() {
-        add(new FilledSprite(WHITE));
+        add(new FilledSprite(Colors.WHITE));
         add(new ImageSprite("success.png", 5, 5));
         add(new Label("Hello World!", 26, 6));
     }
--- templates/scala/src/HelloWorld.scala
+++ templates/scala/src/HelloWorld.scala
@@ -1,5 +1,5 @@
 import pulpcore.animation.Easing._
-import pulpcore.image.Colors._
+import pulpcore.image.Colors
 import pulpcore.scene.Scene2D
 import pulpcore.sprite._
 import PulpCore._
@@ -9,26 +9,26 @@ class HelloWorld extends Scene2D {
   override def load = {
     var sprite = new ImageSprite("success.png", 5, 5)
     
-    add(new FilledSprite(WHITE))
+    add(new FilledSprite(Colors.WHITE))
     add(sprite)
     add(new Label("Hello World!", 26, 6))
     
-    /* 
-    The Java equivalent is: 
-    if (sprite.x.get() == 5) {
-        sprite.x.set(sprite.x.get() + 115);
-        sprite.x.animateTo(5, 1000, Easing.NONE, 1000)
-        // sprite.x.set(200)
-        // sprite.x.animate(120, 5, 1000, Easing.REGULAR_IN, 1000)
-    }
-    */
     if (sprite.x == 5) {
       sprite.x += 115
       sprite.x := 5 dur 1000 delay 1000
       //sprite.x := 200
       //sprite.x := 120 to 5 dur 1000 ease REGULAR_IN delay 1000
     }
-  }
+    /* 
+    The Java equivalent is: 
+    if (sprite.x.get() == 5) {
+        sprite.x.set(sprite.x.get() + 115);
+        sprite.x.animateTo(5, 1000, Easing.NONE, 1000)
+        //sprite.x.set(200)
+        //sprite.x.animate(120, 5, 1000, Easing.REGULAR_IN, 1000)
+    }
+    */
+}
     
   override def update(elapsedTime:Int) = {
       
