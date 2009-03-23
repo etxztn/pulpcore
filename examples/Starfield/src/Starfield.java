89802403d141f611450ac4731b558b3a0a269a2f
brackeen <brackeen@localhost>
2009-03-23 01:27:59 +0000

    Added motion blur to Starfield

--- examples/Starfield/src/Starfield.java
+++ examples/Starfield/src/Starfield.java
@@ -1,12 +1,13 @@
 // Starfield
-// Scroll the mouse wheel or press the arrow keys to travel through the cosmos
-import pulpcore.CoreSystem;
+// Scroll the mouse wheel or press the arrow keys to travel through the cosmos.
+// Shows mouse wheel input, custom sprites, and motion blur.
 import pulpcore.image.BlendMode;
 import pulpcore.Input;
 import pulpcore.math.CoreMath;
 import pulpcore.scene.Scene2D;
 import pulpcore.sprite.ImageSprite;
 import pulpcore.Stage;
+import pulpcore.image.filter.MotionBlur;
 
 public class Starfield extends Scene2D {
     
@@ -30,19 +31,20 @@ public class Starfield extends Scene2D {
     @Override
     public void update(int elapsedTime) {
         if (Input.getMouseWheelRotation() > 0) {
-            zoom(Input.getMouseWheelX(), Input.getMouseWheelY(), -.01);
+            zoom(Input.getMouseWheelX(), Input.getMouseWheelY(), -.02);
         }
         else if (Input.getMouseWheelRotation() < 0) {
-            zoom(Input.getMouseWheelX(), Input.getMouseWheelY(), 1);
-        }
-        
-        if (Input.isDown(Input.KEY_UP)) {
-            zoom(Stage.getWidth() / 2, Stage.getHeight() / 2, 1);
+            zoom(Input.getMouseWheelX(), Input.getMouseWheelY(), 0.75);
         }
         
         if (Input.isDown(Input.KEY_DOWN)) {
-            zoom(Stage.getWidth() / 2, Stage.getHeight() / 2, -.01);
+            zoom(Stage.getWidth() / 2, Stage.getHeight() / 2, -.02);
         }
+
+        if (Input.isDown(Input.KEY_UP)) {
+            zoom(Stage.getWidth() / 2, Stage.getHeight() / 2, 0.75);
+        }
+        
         zoomTimeRemaining -= elapsedTime;
     }
     
@@ -50,17 +52,20 @@ public class Starfield extends Scene2D {
         zoomX = x;
         zoomY = y;
         zoomSpeed = speed;
-        zoomTimeRemaining = 200;
+        zoomTimeRemaining = 300;
     }
     
     class Star extends ImageSprite {
-        
+
+        MotionBlur motionBlur = new MotionBlur();
         double z;
         
         public Star(String image) {
             super(image, CoreMath.rand(0, Stage.getWidth()), CoreMath.rand(0, Stage.getHeight()));
             setZ(CoreMath.rand(1.0, 4.0));
             setBlendMode(BlendMode.Add());
+            setFilter(motionBlur);
+            motionBlur.distance.set(0);
         }
         
         @Override
@@ -68,6 +73,7 @@ public class Starfield extends Scene2D {
             super.update(elapsedTime);
             
             if (zoomTimeRemaining > 0) {
+                double motionBlurDistance = zoomSpeed * z * z * 1.5;
                 double speed = zoomSpeed * z * z / 3000;
                 double dx = (x.get() - zoomX) * elapsedTime * speed;
                 double dy = (y.get() - zoomY) * elapsedTime * speed;
@@ -81,7 +87,23 @@ public class Starfield extends Scene2D {
                     setLocation(
                         zoomX + CoreMath.rand(-Stage.getWidth() / 10, Stage.getWidth() / 10),
                         zoomY + CoreMath.rand(-Stage.getHeight() / 10, Stage.getHeight() / 10));
+                    motionBlur.distance.set(0);
                 }
+                else {
+                    motionBlur.angle.set(Math.atan2(dy, dx));
+                    if (zoomTimeRemaining <= 100) {
+                        // Wind down the motion blur
+                        if (!motionBlur.distance.isAnimating()) {
+                            motionBlur.distance.animateTo(0, zoomTimeRemaining);
+                        }
+                    }
+                    else {
+                        motionBlur.distance.set(motionBlurDistance);
+                    }
+                }
+            }
+            else {
+                motionBlur.distance.set(0);
             }
         }
         
@@ -90,6 +112,5 @@ public class Starfield extends Scene2D {
             setSize(z * 5, z * 5);
             alpha.set(z >= 2 ? 255 : (int)(255 * (z-1)));
         }
-    }
-    
+    }    
 }
