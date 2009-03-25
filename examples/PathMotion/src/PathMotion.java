f27707200814bc18ce3fe82f70c6e66c9e4f4076
brackeen <brackeen@localhost>
2009-03-25 18:08:59 +0000

    Added description

--- examples/PathMotion/src/PathMotion.java
+++ examples/PathMotion/src/PathMotion.java
@@ -1,3 +1,5 @@
+// Shows how to animate a Sprite along a Path.
+// Paths are defined with SVG path data.
 import pulpcore.animation.Timeline;
 import pulpcore.scene.Scene2D;
 import pulpcore.sprite.FilledSprite;
@@ -8,7 +10,8 @@ import pulpcore.sprite.Sprite;
 
 public class PathMotion extends Scene2D {
 
-    // SVG path created with InkScape
+    // SVG path created with InkScape.
+    // M, C, and L commands are supported.
     String svgPath = "M 181.42857,41.428571 C -13.491814,41.428571 -0.00016036284,503.14876 " +
             "178.57143,439.99999 C 319.88994,390.02513 331.55729,390.01741 465.71429,438.57143 " +
             "C 637.82839,500.86285 662.71309,41.428571 448.57142,41.428571 C 102.53673,41.428571 " +
@@ -45,4 +48,3 @@ public class PathMotion extends Scene2D {
         addTimeline(timeline);
     }
 }
-
