3a37c5e1a66deeccd27996e365410a5519ef3f3d
brackeen <brackeen@localhost>
2009-04-13 16:49:46 +0000

    Keep sprites without Stage bounds for demo

--- examples/ScalaLanguage/src/ScalaLanguage.scala
+++ examples/ScalaLanguage/src/ScalaLanguage.scala
@@ -1,5 +1,5 @@
-// Drag the block block around
-// Shows how to use functions, traits, and animation in PulpCore and Scala.
+// Drag the blue block around
+// Shows how to use functions, traits, and animation in Scala.
 import pulpcore.animation.Easing._
 import pulpcore.image.Colors
 import pulpcore.scene.Scene2D
@@ -15,16 +15,16 @@ class ScalaLanguage extends Scene2D {
         // Function binding
         val leftPaddle = new ImageSprite("Block1.png", 0, 0)
         leftPaddle.setAnchor(Sprite.CENTER)
-        leftPaddle.x.bindTo(() => Math.pow(leftPaddle.y - 240, 2) / 2000)
-        leftPaddle.y.bindTo(() => Input.getMouseY)
+        leftPaddle.x.bindTo(() => Math.pow(leftPaddle.y - 240, 2) / 1000 + 64)
+        leftPaddle.y.bindTo(() => Input.getMouseY * 0.73 + 64)
         add(leftPaddle)
 
         // Timeline animation
-        val rightPaddle = new ImageSprite("Block2.png", 640, 0)
+        val rightPaddle = new ImageSprite("Block2.png", 575, 0)
         rightPaddle.setAnchor(Sprite.CENTER)
         val timelineY = Timeline(
-            rightPaddle.y -> (0 to 480 dur 1500),
-            rightPaddle.y -> (480 to 0 dur 1500 delay 1500))
+            rightPaddle.y -> (64 to 416 dur 1500),
+            rightPaddle.y -> (416 to 64 dur 1500 delay 1500))
         timelineY.loopForever()
         addTimeline(timelineY)
         add(rightPaddle)
