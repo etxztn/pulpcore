82d1fe1de9e97f44a537960a43bfd24c1adb48d1
brackeen <brackeen@localhost>
2009-11-16 13:33:48 -0800

    Added missing jar folder, Updated hgignore to ignore API docs

--- src/pulpcore/Build.java
+++ src/pulpcore/Build.java
@@ -13,7 +13,7 @@
             public static final String VERSION = "0.11.6";
             
             /** The build date. */
-            public static final String BUILD_DATE = "2009/11/15 21:58:28";
+            public static final String BUILD_DATE = "2009/11/16 13:24:10";
             
             /** The platform name of this PulpCore build. */
             public static final String PLATFORM = "applet";
